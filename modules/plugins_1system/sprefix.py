3ab998c50c10e1e30454b2043cd60bcb9f25d0a6
A9FM [aka Artur Lamerov] <143527536+a9-fm@users.noreply.github.com>
2025-08-07 00:42:02 +0300

    фикс месседж.комманд

--- modules/plugins_1system/sprefix.py
+++ modules/plugins_1system/sprefix.py
@@ -13,9 +13,13 @@ config.read(PATH_FILE)
 @Client.on_message(fox_command("sp", "SetPrefix", os.path.basename(__file__), "[new prefix]") & fox_sudo())
 async def sprefix(client, message):
     message = await who_message(client, message)
-    prefixgett = message.command[1]
-    config.set("prefix", "prefix", prefixgett)
-    with open(PATH_FILE, "w") as config_file:
-        config.write(config_file)
-    await message.edit(f"<emoji id='5237699328843200968'>✅</emoji> <b>prefix [ <code>{prefixgett}</code> ] set!</b>\n<emoji id='5264727218734524899'>🔄</emoji> Restarting userbot...")
-    await restart(message, restart_type="restart")
+    if len(message.text.split()) > 1:
+        prefixgett = message.text.split()[1]
+        config.set("prefix", "prefix", prefixgett)
+        with open(PATH_FILE, "w") as config_file:
+            config.write(config_file)
+        await message.edit(f"<emoji id='5237699328843200968'>✅</emoji> <b>prefix [ <code>{prefixgett}</code> ] set!</b>\n<emoji id='5264727218734524899'>🔄</emoji> Restarting userbot...")
+        await restart(message, restart_type="restart")
+    else:
+        await message.edit("<b>prefix don't be None</b>")
+
